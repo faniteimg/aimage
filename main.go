4015032c1621ced420c2cfc0703f23cdf855bfca
faniteimg <64721615+faniteimg@users.noreply.github.com>
2024-01-17 05:58:59 +0800

    Update main.go

--- main.go
+++ main.go
@@ -17,7 +17,7 @@ var innerHtml = `
     <style>html,body{margin:0px;background:#0e0e0e;display:flex;justify-content:center;align-items:center;height:100vh;width:100vw;}img{max-height:95vh;width:auto;-webkit-user-select:none;}</style>
 </head>
 <body>
-    <img src="%s">
+    <img alt="美女" title="美女" src="%s">
 </body>
 </html>
 `
