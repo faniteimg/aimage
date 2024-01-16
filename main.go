8bb272f45ffccac441297db596d851593fc8d308
faniteimg <64721615+faniteimg@users.noreply.github.com>
2024-01-17 04:58:47 +0800

    Update main.go

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"net/http"
+	"os"
 )
 
 var innerHtml = `
@@ -16,13 +17,21 @@ var innerHtml = `
     <style>html,body{margin:0px;background:#0e0e0e;display:flex;justify-content:center;align-items:center;height:100vh;width:100vw;}img{max-height:95vh;width:auto;-webkit-user-select:none;}</style>
 </head>
 <body>
-    <img src="https://s2.loli.net/2022/07/29/kRVJgN795QFrxjO.jpg">
+    <img src="%s">
 </body>
 </html>
 `
 
+func getEnv(key, fallback string) string {
+    if value, ok := os.LookupEnv(key); ok {
+        return value
+    }
+    return fallback
+}
+
 func index(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, innerHtml)
+	imageUrl := getEnv("IMAGE_URL", "https://jsd.nn.ci/gh/faniteimg/images@master/img/36d05a1add1d078a75438ba833cf5afd.jpg")
+	fmt.Fprintf(w, innerHtml, imageUrl)
 }
 
 func main()  {
